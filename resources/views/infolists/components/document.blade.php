3dbdb9f4f718f66be9e7e2b755435c5b601a56d3
dupont-ext <kyfr59@gmail.com>
2026-03-03 10:03:48 +0100

    Correction d'un bug sur le lien vers une lettre PDF dans l'admin

--- resources/views/infolists/components/document.blade.php
+++ resources/views/infolists/components/document.blade.php
@@ -2,7 +2,7 @@
     <div class="text-sm flex gap-3">
         @foreach($getState() as $index => $document)
             @php
-                $url = Storage::temporaryUrl($document->content->uri, now()->addMinutes(5));
+                $url = Storage::disk('public')->url($document->content->uri, now()->addMinutes(5));
             @endphp
             <a href="{{ $url }}" class="w-full flex items-center justify-center gap-2 py-2 px-3 border border-gray-200 rounded-md" target="_blank">
                 {{ 'Courrier n° ' . ($index + 1) }} <span class="text-xs">({{ round($document->size / 1000) }} Ko)</span>

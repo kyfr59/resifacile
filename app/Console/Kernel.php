5603efdaceaedcf7ac99ddb0517e90252a448096
dupont-ext <kyfr59@gmail.com>
2026-03-09 09:32:16 +0100

    Désactive tous les crons sauf le processus d'envoi à Maileva

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,20 +13,24 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // 10 x 1mn - 5 login per 30mn per host
-        $schedule->command('notification:get')->everyThirtyMinutes()->onOneServer();
+        //$schedule->command('notification:get')->everyThirtyMinutes()->onOneServer();
         //$schedule->command('campaign:execute')->everyFiveMinutes()->onOneServer();
 
-        $schedule->command('mail:send-proof')->everyThirtyMinutes()->onOneServer();
-        $schedule->command('tracking:get')->dailyAt('8:00')->onOneServer();
+        //$schedule->command('mail:send-proof')->everyThirtyMinutes()->onOneServer();
+        //$schedule->command('tracking:get')->dailyAt('8:00')->onOneServer();
 
-        $schedule->command('subscription:invoices')->dailyAt('22:00')->onOneServer();
-        $schedule->command('subscription:renew')->dailyAt('1:00');
+        //$schedule->command('subscription:invoices')->dailyAt('22:00')->onOneServer();
+        //$schedule->command('subscription:renew')->dailyAt('1:00');
 
-        $schedule->command('horizon:snapshot')->everyTenMinutes()->onOneServer();
+        //$schedule->command('horizon:snapshot')->everyTenMinutes()->onOneServer();
 
-        $schedule->command('sitemap:generate')->daily()->onOneServer();
+        //$schedule->command('sitemap:generate')->daily()->onOneServer();
 
-        $schedule->command('telescope:prune --hours=48')->daily();
+        //$schedule->command('telescope:prune --hours=48')->daily();
+        $schedule->command('sending:single 1')
+            ->everyMinute()
+            ->emailOutputOnFailure('dev@resifacile.fr');
+
     }
 
     /**

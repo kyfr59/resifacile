bdacd3ba671124947debb6d3a91e2f496a1f9f2f
dupont-ext <kyfr59@gmail.com>
2026-05-26 10:20:12 +0200

    Masque les champs du bypass de paiement

--- resources/views/livewire/letter-fake-payment-buttons.blade.php
+++ resources/views/livewire/letter-fake-payment-buttons.blade.php
@@ -1,19 +1,19 @@
 @inject('accounting', 'App\Helpers\Accounting')
 <form method="POST" action="/webhook-hipay">
     @csrf
-    <input type="text" name="status" value="118" />
-    <input type="text" name="transaction_reference" value="TX123456" />
-    <input type="text" name="mid" value="MERCHANT123" />
+    <input type="hidden" name="status" value="118" />
+    <input type="hidden" name="transaction_reference" value="TX123456" />
+    <input type="hidden" name="mid" value="MERCHANT123" />
     <br><br>
-    <br><br>with_subscription : <input type="text" name="custom_data[with_subscription]" value="{{ $orderInfos['with_subscription'] }}" />
-    <br><br>has_subscription : <input type="text" name="custom_data[has_subscription]" value="{{ $orderInfos['has_subscription'] }}" />
+    <br><br><input type="hidden" name="custom_data[with_subscription]" value="{{ $orderInfos['with_subscription'] }}" />
+    <br><br><input type="hidden" name="custom_data[has_subscription]" value="{{ $orderInfos['has_subscription'] }}" />
     <br>
-    <br><br>is_subscription_transaction : <input type="text" name="custom_data[is_subscription_transaction]" value="" />
-    <br><br>subscription_id : <input type="text" name="custom_data[subscription_id]" value="" />
-    <br><br><input type="text" name="custom_data[order_id]" value="{{ $order->id }}" />
-    <input type="text" name="custom_data[customer_id]" value="{{ $order->customer_id }}" />
+    <br><br><input type="hidden" name="custom_data[is_subscription_transaction]" value="" />
+    <br><br><input type="hidden" name="custom_data[subscription_id]" value="" />
+    <br><br><input type="hidden" name="custom_data[order_id]" value="{{ $order->id }}" />
+    <input type="hidden" name="custom_data[customer_id]" value="{{ $order->customer_id }}" />
     <input
-        type="text"
+        type="hidden"
         name="authorized_amount"
         value="{{ number_format($accounting::addTax($order->amount) / 100, 2, '.', '') }}"
     />

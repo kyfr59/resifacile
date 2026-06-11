40e6402eb1b0ff77c40eee16c64005d36a31b3a9
dupont-ext <kyfr59@gmail.com>
2026-06-11 10:47:01 +0200

    Gestion du statut PROCESSED_WITH_ERRORS chez Maileva

--- app/Enums/MailevaStatus.php
+++ app/Enums/MailevaStatus.php
@@ -5,8 +5,10 @@ namespace App\Enums;
 enum MailevaStatus: string
 {
     case ON_STATUS_ACCEPTED = 'ON_STATUS_ACCEPTED';
-    case ON_STATUS_REJECTTED = 'ON_STATUS_REJECTED';
+    case ON_STATUS_REJECTED = 'ON_STATUS_REJECTED';
     case ON_STATUS_PROCESSED = 'ON_STATUS_PROCESSED';
+    case ON_STATUS_PROCESSED_WITH_ERRORS = 'ON_STATUS_PROCESSED_WITH_ERRORS';
+
 
     public function label(): string
     {
@@ -15,6 +17,7 @@ enum MailevaStatus: string
             self::ON_STATUS_ACCEPTED => "Envoi accepté",
             self::ON_STATUS_REJECTED => "Envoi rejeté",
             self::ON_STATUS_PROCESSED => "Envoi traité",
+            self::ON_STATUS_PROCESSED_WITH_ERRORS => "Envoi traité avec des erreurs",
         };
     }
 
@@ -25,6 +28,7 @@ enum MailevaStatus: string
             self::ON_STATUS_ACCEPTED => "L'envoi a été accepté par la plateforme Maileva.",
             self::ON_STATUS_REJECTED => "L'envoi a été rejeté par la plateforme Maileva.",
             self::ON_STATUS_PROCESSED => "L'envoi a été traité par la plateforme Maileva.",
+            self::ON_STATUS_PROCESSED_WITH_ERRORS => "L'envoi a été traité par la plateforme Maileva mais comporte des erreurs.",
         };
     }
 
--- config/webhook-client.php
+++ config/webhook-client.php
@@ -48,6 +48,7 @@ return [
                 'processed' => \App\Jobs\MailevaWebhooks\HandleProcessed::class,
                 'accepted' => \App\Jobs\MailevaWebhooks\HandleAccepted::class,
                 'rejected' => \App\Jobs\MailevaWebhooks\HandleRejected::class,
+                'processed_with_errors' => \App\Jobs\MailevaWebhooks\HandleProcessedWithErrors::class,
             ],
         ]
     ],

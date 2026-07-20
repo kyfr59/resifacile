f5b7c1d7aabc2df1af3e699862d7a2dce88899a4
dupont-ext <kyfr59@gmail.com>
2026-07-20 10:27:39 +0200

    Modification du corps du mail de confirmation de résiliation

--- resources/views/emails/unsubscribe-confirmation-text.blade.php
+++ resources/views/emails/unsubscribe-confirmation-text.blade.php
@@ -1,11 +1,17 @@
-Bonjour,
+Chère cliente, cher client,
 
-Nous vous confirmons que nous avons bien enregistré votre demande et que votre résiliation a bien été effectuée.
+Nous vous informons que votre demande de résiliation a bien été enregistrée et traitée conformément à vos souhaits. Votre abonnement est désormais résilié.
 
-Conformément au droit applicable, les données rattachées à votre dossier seront automatiquement supprimées de nos systèmes à l’issue du délai de prescription légale.
+La résiliation de votre abonnement n’entraîne toutefois pas la suppression de votre compte Resifacile. Vous conservez ainsi l’accès à votre espace client et pouvez continuer à consulter vos factures, vos lettres envoyées ainsi que vos preuves de contenu et de dépôt.
 
-Si vous avez souscrit par erreur à cet abonnement, nous vous invitons à consulter nos offres unitaires spécialement conçues pour les envois ponctuels. N'hésitez pas à en profiter.
-Nous espérons avoir le plaisir de vous accueillir à nouveau sur notre plateforme.
+Si vous souhaitez également supprimer définitivement votre compte, rendez-vous dans votre espace client et cliquez sur « Supprimer mon compte ». Une fois la suppression effectuée, un nouvel e-mail vous sera envoyé afin de vous confirmer la suppression définitive de votre compte.
+
+Si votre souscription à notre service a été effectuée par erreur, nous tenons à vous rappeler que nous proposons des offres unitaires spécialement conçues pour les envois ponctuels. Nous vous invitons à consulter nos options disponibles et à en profiter selon vos besoins.
+
+Nous espérons sincèrement que cette expérience n’a pas nui à votre satisfaction globale avec nos services. En cas de questions supplémentaires ou si vous avez besoin d’assistance, n’hésitez pas à nous contacter. Nous sommes là pour vous aider.
+
+Nous souhaitons également exprimer notre espoir de vous accueillir à nouveau sur notre plateforme à l’avenir. Nous sommes déterminés à continuer à vous offrir des services de qualité qui répondent à vos attentes.
 
 Bien à vous,
-L'équipe de {{ config('app.name') }}
+
+L’équipe de Resifacile.fr
--- resources/views/emails/unsubscribe-confirmation.blade.php
+++ resources/views/emails/unsubscribe-confirmation.blade.php
@@ -1,16 +1,19 @@
 <x-mail::message>
 Chère cliente, cher client,
 
-Nous vous informons par la présente que votre demande de résiliation a été enregistrée avec succès. Votre résiliation a été traitée conformément à vos souhaits.
+Nous vous informons que votre demande de résiliation a bien été enregistrée et traitée conformément à vos souhaits. Votre abonnement est désormais résilié.
 
-Conformément aux dispositions légales en vigueur, toutes les données associées à votre dossier seront automatiquement supprimées de nos systèmes à l’issue du délai de prescription légale.
+La résiliation de votre abonnement n’entraîne toutefois pas la suppression de votre compte Resifacile. Vous conservez ainsi l’accès à votre espace client et pouvez continuer à consulter vos factures, vos lettres envoyées ainsi que vos preuves de contenu et de dépôt.
+
+Si vous souhaitez également supprimer définitivement votre compte, rendez-vous dans votre espace client et cliquez sur « Supprimer mon compte ». Une fois la suppression effectuée, un nouvel e-mail vous sera envoyé afin de vous confirmer la suppression définitive de votre compte.
 
 Si votre souscription à notre service a été effectuée par erreur, nous tenons à vous rappeler que nous proposons des offres unitaires spécialement conçues pour les envois ponctuels. Nous vous invitons à consulter nos options disponibles et à en profiter selon vos besoins.
 
-Nous espérons sincèrement que cette expérience n'a pas nui à votre satisfaction globale avec nos services. En cas de questions supplémentaires ou si vous avez besoin d'assistance, n'hésitez pas à nous contacter. Nous sommes là pour vous aider.
+Nous espérons sincèrement que cette expérience n’a pas nui à votre satisfaction globale avec nos services. En cas de questions supplémentaires ou si vous avez besoin d’assistance, n’hésitez pas à nous contacter. Nous sommes là pour vous aider.
 
-Nous souhaitons également exprimer notre espoir de vous accueillir à nouveau sur notre plateforme à l'avenir. Nous sommes déterminés à continuer à vous offrir des services de qualité qui répondent à vos attentes.
+Nous souhaitons également exprimer notre espoir de vous accueillir à nouveau sur notre plateforme à l’avenir. Nous sommes déterminés à continuer à vous offrir des services de qualité qui répondent à vos attentes.
 
-Bien à vous,<br>
-L'équipe de {{ config('app.name') }}
+Bien à vous,
+
+L’équipe de Resifacile.fr
 </x-mail::message>
